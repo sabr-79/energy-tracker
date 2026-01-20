a921dfcd76be70bff5f7959c8e190580f305f79e
sabr_79 <sabirinm07976@gmail.com>
2026-01-19 18:32:15 -0600

    unneeded semicolon in app.jsx return statement.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import axios from "axios"; 
 
 function App() {
+
+  // async == takes time. allows for await, .get is for HTTP get request to the backend
   const pingBackend = async () =>{
     const res = await axios.get("http://127.0.0.1:8000/ping");
 
+    // res.data = json backend response, res.status = http status code, res.headers = response headers
+    // prints to browser console
     console.log(res.data);
   }
   return (
