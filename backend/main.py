972602216e46d9465ff41178b72ecb6e5dbbe978
sabr_79 <sabirinm07976@gmail.com>
2026-01-19 18:31:23 -0600

    updated app.jsx for frontend-backend communication, added comments

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,33 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 
-@app.get('/')
+# CORS: this enables the frontend to request info from backend, otherwise it will be blocked
+app.add_middleware(
+    CORSMiddleware,
 
+    # currently hosts the frontend
+    allow_origins = ["http://localhost:5173"],
+
+    # for future use, allows requests to include cookies, auth tokens, session info for login
+    allow_credentials=True,
+    
+    # allows all http methods
+    allow_methods = ["*"],
+
+    # for jwt auth later, also for custom headers
+    allow_headers=["*"],
+
+)
+
+# default msg in backend
+@app.get('/')
 def index():
     return {"Msg" : "Hello, world!"}
 
+# returns if the button is clicked in frontend
 @app.get("/ping")
 def ping():
     return {"status": "ok"}
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,17 @@
+import axios from "axios"; 
+
 function App() {
-  return <h1>Hello, world!</h1>;
+  const pingBackend = async () =>{
+    const res = await axios.get("http://127.0.0.1:8000/ping");
+
+    console.log(res.data);
+  }
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <button onClick ={pingBackend}>Ping backend</button>
+    </div>
+    );
   
 }
 
